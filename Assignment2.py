fc2a87952877354cac45948cf274ce4de73dbae0
Murtja <murtja.alkhabbaz@uoit.net>
2017-02-28 19:11:34 -0500

    bug fixing

--- Assignment2.py
+++ Assignment2.py
@@ -1,3 +1,6 @@
+import boto3
+from datetime import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,18 +14,13 @@ def home():
 
 @app.route('/test')
 def test():
-    import boto3
-    from datetime import datetime
-
-    client = boto3.client('cloudwatch')
-
     cloudwatch = boto3.resource('cloudwatch')
     metric = cloudwatch.Metric('AWS/EC2', 'CPUUtilization')
 
     stats = metric.get_statistics(
         StartTime=datetime(2017, 2, 27),
         EndTime=datetime(2017, 3, 1),
-        Period=300,
+        Period=60,
         Statistics=['Sum']
     )
 
