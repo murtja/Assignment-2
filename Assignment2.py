7804d98aa0801bc4c55e42a0bff69ebba0a3c5db
Murtja <murtja.alkhabbaz@uoit.net>
2017-02-28 19:03:57 -0500

    added debug mode

--- Assignment2.py
+++ Assignment2.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route('/')
@@ -28,4 +29,4 @@ def test():
     return render_template('test.html', utilitzation=str(stats))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
